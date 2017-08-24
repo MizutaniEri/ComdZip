5bfdc43072b53e302fd317640c369a64253ad66a
水谷絵里 <kirishima.mana.eva@gmail.com>
2017-08-24 17:20:52 +0900

    新規作成

---
+++ ComdZip/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComdZip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <=0)
+            {
+                ZipArchiveDirectory();
+            }
+        }
+
+        private static void ZipArchiveDirectory()
+        {
+            var currDir = System.Environment.CurrentDirectory;
+            Directory.EnumerateDirectories(currDir, "*.*", SearchOption.TopDirectoryOnly).ToList().ForEach(dir => {
+                var zipName = Path.ChangeExtension(dir, ".zip");
+                ZipFile.CreateFromDirectory(dir, zipName, CompressionLevel.Optimal, false);
+                Console.WriteLine(zipName + " ...Complete!");
+            });
+        }
+    }
+}
